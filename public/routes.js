b18b76ab268e6abdf1594ced271bb894c375ea26
Kelsie <kelsie51@hotmail.com>
2021-04-30 00:54:36 -0500

    Added api routes. Added screenshot. Referenced Models in Routes file.

--- public/routes.js
+++ public/routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const Workout = require("../models");
 const path = require("path");
 
 
@@ -13,3 +14,60 @@ router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
+router.get("/api/workouts/", async (req, res) => {
+    try {
+        const data = await Workout.find({})
+        const returnData = [];
+
+        data.forEach(item => {
+            const workout = new Workout(item)
+            workout.exercises = item.exercises
+            workout.lastDuration();
+            returnData.push(workout);
+        })
+
+        res.json(returnData);
+
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+});
+router.post("/api/workouts/", async (req, res) => {
+    try {
+        const data = await Workout.create({});
+        res.json(data);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+});
+
+router.put("/api/workouts/:id", async (req, res) => {
+    try {
+        const data = await Workout.findOneAndUpdate(
+            {
+                _id: req.params.id
+            },
+            {
+                $push: {
+                    exercises: req.body
+                }
+            },
+        );
+        res.json(data);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+});
+router.get("/api/workouts/range", async (req, res) => {
+    try {
+        const data = await Workout.find({})
+        res.json(data);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+});
+module.exports = router
